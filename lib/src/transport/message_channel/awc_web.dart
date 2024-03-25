c0a520f326bf4f29915292d96a74153985e2973f
Chralu <contact@charly-caulet.net>
2024-03-25 15:19:23 +0100

    ✨ Add MessageChannel RPC for web clients.

---
+++ lib/src/transport/message_channel/awc_web.dart
@@ -0,0 +1,27 @@
+@JS()
+library awc;
+
+import 'dart:async';
+import 'dart:html';
+
+import 'package:js/js.dart';
+
+external MessagePort? get awc;
+external bool? get awcAvailable;
+
+@JS('onAWCReady')
+external set onAWCReady(void Function(MessagePort awc) f);
+
+Future<MessagePort> get asyncAWC async {
+  if (awc != null) return awc!;
+  print('Wait for awc');
+  final awcReadyCompleter = Completer<MessagePort>();
+
+  if (awc != null) awcReadyCompleter.complete(awc!);
+
+  onAWCReady = allowInterop((awc) {
+    print('AWC ready !');
+  });
+
+  return awcReadyCompleter.future;
+}
